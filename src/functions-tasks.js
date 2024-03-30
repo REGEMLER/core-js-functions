7b26702b1cd8cb9a4fd0598806a080e6a4ef0a29
Yar Night <yaroslav239@gmail.com>
2024-03-30 10:01:02 +0300

    feat: add task10

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -199,8 +199,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  return () => {
+    const currentCounter = counter;
+    counter = currentCounter + 1;
+    return currentCounter;
+  };
 }
 
 module.exports = {
